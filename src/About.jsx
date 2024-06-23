1ed7dd29bb763fe328556cf372538f3554fb4c38
VISHU9918 <vk9628117@gmail.com>
2024-06-23 14:57:54 +0530

    Resolved conflicts

---
+++ src/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import  web from "../src/images/about.jpg";
+import Common from './Common';
+
+const About = () => {
+  return (
+    <>
+     <Common 
+     name='Welcome to About Page' 
+     imgsrc={web} 
+     visit='/contact' 
+     btname="Contact Now"
+     />
+    </>
+  );
+};
+
+export default About;
